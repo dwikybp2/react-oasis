b12b226c5705114afd3742f5798e3fadb676425c
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-09-25 09:22:11 +0700

    feat: set routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
-import styled from "styled-components"
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
-const StyledApp = styled.div`
-padding: 20px;
-`;
+import GlobalStyles from './styles/GlobalStyles'
+import Dashboard from './pages/Dashboard'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row type='horizontal'>
-          <Heading as='h1'>THe wild</Heading>
-
-          <div>
-          <Heading as='h2'>THe wild</Heading>
-          <Button onClick={() => alert('Check In')}>Check In</Button>
-          <Button onClick={() => alert('Check Out')}>Check In</Button>
-          </div>
-        </Row>
-
-        <Row type='vertical'>
-          <Heading as='h3'>THe wild</Heading>
-
-          <form>
-          <Input type="number" placeholder="Number of guest" />
-          <Input type="number" placeholder="Number of guest" />
-          </form>
-        </Row>
-      </StyledApp>
+    <GlobalStyles />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to='dashboard' />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='cabins' element={<Cabins />} />
+        <Route path='users' element={<Users />} />
+        <Route path='settings' element={<Settings />} />
+        <Route path='account' element={<Account />} />
+        <Route path='login' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
