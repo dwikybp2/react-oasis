d9a3a822edcca5b5527b74bdba4f8939c2e112f7
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-10 09:29:55 +0700

    feat: filter cabin

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
-
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>Filter / Sort</p>
+      <CabinTableOperations />
       
     </Row>
 
