fd7f6b70e3709d02dbc8893a1644637c05c9117d
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-12 17:06:58 +0700

    feat: resusable pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -2,6 +2,7 @@ import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
+import Pagination from "../../ui/Pagination";
 
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
@@ -31,6 +32,10 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
+        <Table.Footer>
+          <Pagination count={15} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
