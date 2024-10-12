6d8c71ad0787443ce792484aa80adfe40b1eb792
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-12 13:46:11 +0700

    feat: bookings filter (server side)

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,23 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase.from('bookings')
+    .select('id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)');
+  
+  //filter
+  if(filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value);
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -73,7 +73,7 @@ function MainNav() {
         <li>
             <StyledNavLink to="/users">
               <HiOutlineUsers />
-              <span>Bookings</span>
+              <span>Users</span>
             </StyledNavLink>
           </li>
         <li>
