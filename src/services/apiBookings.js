976085fc355ac82e842432e4159906549a2a7ecd
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-12 16:45:29 +0700

    feat: booking sort

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -6,7 +6,9 @@ export async function getBookings({ filter, sortBy }) {
     .select('id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)');
   
   //filter
-  if(filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value);
+  if(filter) query = query[filter.method || 'eq'](filter.field, filter.value);
+  
+  if(sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === 'asc'});
 
   const { data, error } = await query;
 
