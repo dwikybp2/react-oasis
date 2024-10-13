de4f99beab044361f29e8f07f4027eaf804dfa42
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-14 06:21:43 +0700

    feat: checkout booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -11,6 +11,8 @@ import ButtonText from "../../ui/ButtonText";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -20,6 +22,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const moveBack = useMoveBack();
   const navigate = useNavigate();
@@ -48,6 +51,11 @@ function BookingDetail() {
 
       <ButtonGroup>
       {status === 'unconfirmed' && <Button onClick={() => navigate(`/checkin/${bookingId}`)} >Check In</Button>}
+      
+      {status === 'checked-in' && 
+            <Button icon={<HiArrowUpOnSquare />} onClick={() => checkout(bookingId)} disabled={isCheckingOut} >
+                Check Out
+            </Button>}
 
         <Button variation="secondary" onClick={moveBack}>
           Back
