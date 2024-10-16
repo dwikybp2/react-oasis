c3747eb68399b5d3fa32d1d26811552e72775f9d
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-17 06:00:27 +0700

    fet: logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -2,7 +2,6 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from "react-router";
 import toast from "react-hot-toast";
-import supabase from "../../services/supabase";
 
 export function useLogin() {
     const queryClient = useQueryClient();
@@ -12,8 +11,8 @@ export function useLogin() {
         mutationFn: ({ email, password }) => loginApi({ email, password }),
 
         onSuccess: (user) => {
-            queryClient.setQueriesData(['user'], user);
-            navigate('/dashboard');
+            queryClient.setQueryData(['user'], user.user);
+            navigate('/dashboard', {replace: true});
         },
 
         onError: (error) => {
@@ -23,17 +22,4 @@ export function useLogin() {
     });
 
     return { login, isLoading };
-}
-
-export async function getCurrentUser() {
-    const { data: session } = await supabase.auth.getSession();
-
-    if(!session.session) return null;
-
-    const { data, error } = await supabase.auth.getUser();
-
-    console.log(data);
-    if (error) throw new Error(error.message);
-
-    return data?.user;
 }
