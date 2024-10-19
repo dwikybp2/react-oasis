66851ff3d781f15fab9aa87ba70b84e8fc848f96
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-10-19 14:47:25 +0700

    feat: dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import AppLayout from "./ui/AppLayout"
 import Booking from "./pages/Booking"
 import Checkin from "./pages/Checkin"
 import ProtectedRoute from "./ui/ProtectedRoute"
+import { DarkModeProvider } from "./context/DarkModeContext"
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,6 +29,7 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
+    <DarkModeProvider>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
 
@@ -71,6 +73,7 @@ function App() {
       } 
     }} />
     </QueryClientProvider>
+    </DarkModeProvider>
   )
 }
 
